92185dbe87068256cb5f89b7d91ba92c9a930a15
Sujal Tangde <sujaltagade01@gmail.com>
2023-09-02 17:03:29 +0530

    completed profile ui

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,54 +1,131 @@
 import React from 'react'
-import {MetaData} from '../components/MetaData'
-import {useDispatch, useSelector} from 'react-redux'
-import {Loader} from '../components/Loader'
+import { MetaData } from '../components/MetaData'
+import { useDispatch, useSelector } from 'react-redux'
+import { Loader } from '../components/Loader'
+import {Link} from 'react-router-dom'
+
 
 export const MyProfile = () => {
 
-  const {loading, me} = useSelector(state => state.user)
+  const { loading, me } = useSelector(state => state.user)
   console.log(me)
+  const convertDateFormat = (inputDate) => {
+    const parts = inputDate.split('-');
+    if (parts.length !== 3) {
+      return "Invalid date format";
+    }
 
+    const day = parts[2];
+    const month = parts[1];
+    const year = parts[0];
+
+    return `${day}-${month}-${year}`;
+  }
   return (
     <>
 
-<MetaData title="My Profile" />
-      <div className='bg-gray-950 min-h-screen pt-14 md:px-20 px-3  text-white'>
+      <MetaData title="My Profile" />
+      <div className='bg-gray-950 min-h-screen pt-14  md:px-20 px-3  text-white'>
         {
-          loading ? <Loader/> : 
+          loading ? <Loader /> :
+            <>
+              <div className='text-left text-3xl  underL absolute   pl-4 underline-offset-8 md:pt-6 pt-3'>
+                <span className=' font-medium  '>My Profile</span>
+              </div>
 
-            <div className='flex justify-center items-center border border-blue-500 min-h-[90vh] ' >
+              <div className='flex md:flex-row md:gap-12 flex-col md:justify-around justify-center items-top md:pt-12  border-blue-500 min-h-[90vh] ' >
 
-                
+
+                <div className=' md:w-1/2 w-full md:pb-0 pt-16  md:pt-10 gap-8 flex flex-col justify-start items-center '>
+                  <div className=' w-72 h-72 flex md:justify-center justify-start items-center  '>
+
+                    <img src={me.avatar.url} className='rounded-full w-full h-full' alt="" />
+                  </div>
+                  <div className='flex justify-center items-center'>
+                    <Link className='blueCol px-10 py-2 font-semibold'>Edit Profile</Link>
+                  </div>
+                </div>
+
+
+
+                <div className='  md:w-1/2 w-full md:px-0 px-4 pb-20 md:pt-4 pt-8'>
+                  <div className='flex flex-col md:gap-5 gap-6'>
+                    <div>
+                      <p className='md:text-2xl text-xl'>Full Name</p>
+                      <p className='md:text-xl pt-1 text-lg'>{me.name}</p>
+                    </div>
+                    <div>
+                      <p className='md:text-2xl text-xl'>Email</p>
+                      <p className='md:text-xl pt-1 text-lg'>{me.email}</p>
+                    </div>
+                    <div>
+                      <p className='md:text-2xl text-xl'>Jonined On</p>
+                      <p className='md:text-xl pt-1 text-lg'>{convertDateFormat(me.createdAt.substr(0, 10))}</p>
+                    </div>
+                    <div>
+                      <p className='md:text-2xl text-xl'>Skills</p>
+                      <div className='md:text-xl text-lg pt-3 flex gap-3'>{
+                        me.skills.map((skill, i) => (
+                          <span className='bg-yellow-500 text-black  text-sm px-2 py-1  font-bold '>{skill}</span>
+                        ))
+                      }</div>
+                    </div>
+
+                    <div className='flex md:flex-row flex-col md:gap-8 pt-4 gap-3'>
+                      <ul className='flex flex-col gap-4'>
+                        <li className=' '> <Link  ><button className='blueCol w-2/3 md:w-full  font-medium px-6 py-1'>My Resume</button></Link> </li>
+                        <li className=' '>
+                          <Link><button className='blueCol w-2/3 md:w-full font-medium px-6 py-1'>My Applications</button></Link>
+                        </li>
+                        <li className=' '>
+                          <Link><button className='blueCol w-2/3 md:w-full font-medium px-6 py-1'>Saved Jobs</button></Link>
+                        </li>
                         
+                      </ul>
+                      <ul className='flex flex-col gap-4'>
+                        <li className=' '>
+                          <Link><button className='blueCol w-2/3 md:w-full font-medium px-6 py-1'>Change Password</button></Link>
+                        </li>
+                        <li className=' '><Link><button className='blueCol w-2/3 md:w-full font-medium px-6 py-1'>Delete Account</button></Link></li>
+                      </ul>
+                      
+                    </div>
+
+                  </div>
+                </div>
+
+              </div>
+
+            </>
 
 
-            </div>
-          
-        
+
+
+
         }
 
 
-        </div>
-    
+      </div>
+
     </>
   )
 }
 
 // _id: "64f2d9a08359986acaa53af1"
-// ​
+//
 // avatar: Object { public_id: "avatar/mkzuwzhg2drkq9epytc8", url: "https://res.cloudinary.com/dwltg98sm/image/upload/v1693637020/avatar/mkzuwzhg2drkq9epytc8.jpg" }
-// ​
+//
 // createdAt: "2023-09-02T06:43:44.506Z"
-// ​
+//
 // email: "levi@mail.com"
-// ​
+//
 // name: "levi ackerman"
-// ​
+//
 // password: "$2b$10$y8dofrvjZmGj9m1FAViDq.Z0bGkJiikfSbx09iMrOAvd01tuogYSK"
-// ​
+//
 // resume: Object { public_id: "resume/pmthgkompbnjlja3lgy3", url: "https://res.cloudinary.com/dwltg98sm/image/upload/v1693637022/resume/pmthgkompbnjlja3lgy3.png" }
-// ​
+//
 // role: "applicant"
-// ​
+//
 // skills: Array [ "frontend", "backend" ]
 // ​
