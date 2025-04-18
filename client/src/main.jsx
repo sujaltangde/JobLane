0dc8cf0404f5b23f41e1b9527e0f64562d703521
DOMinator.jsx <parthbrajput30@gmail.com>
2025-04-18 15:32:26 +0530

    Fixed the issue of the home and logo route scroll on the top of the page.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './store.js'
-import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTopWhenRouteChanges from "./components/ScrollToTopOnRouteChange.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <Router>
+      <ScrollToTopWhenRouteChanges />
       <App />
     </Router>
   </Provider>
-)
+);
